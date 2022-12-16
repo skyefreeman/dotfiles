f69a328b4ba16bb8ff0bc846e29347f8c634ff5d
skye <skyefreeman@icloud.com>
2022-12-15 20:04:21 -0500

    Fix startup.

--- sbclrc.lisp
+++ sbclrc.lisp
@@ -1,6 +1,6 @@
 (defpackage :skyetools
   (:use :common-lisp)
-  (:export :start-ssk))
+  (:export #:start-ssk))
 
 (in-package :skyetools)
 
@@ -13,7 +13,7 @@
 
 (defun start-ssk ()
   "Loads the SwiftStarterKits system and starts the server for development." 
-  (ql:quickload :starterkit-web)
+  (ql:quickload "starterkit-web")
   (if (not (starterkit-web:running?))
       (starterkit-web:start :port 8080))
   (in-package :starterkit-web))
@@ -22,3 +22,5 @@
 ;; todo: save this file in dotfiles
 ;; todo: add this file as part of new machine setup
 
+
+(in-package :cl-user)
