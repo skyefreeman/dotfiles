d4ee2bd572736e2991a80356bcba9d1bfa0c4121
skye <skyefreeman@icloud.com>
2023-03-25 13:13:09 -0400

    save work.

--- sbclrc.lisp
+++ sbclrc.lisp
@@ -1,26 +1,12 @@
-(defpackage :skyetools
-  (:use :common-lisp)
-  (:export #:start-ssk))
-
-(in-package :skyetools)
-
-;; How to: Load the current file and sync the repl
+;;;; How to:
 ;;
+;;  Load the current file and sync the repl
 ;;  C-c ~ (sly-mprepl-sync)
-;;
 
-;; SwiftStarterKits
+;;;; Default Packages
 
-(defun start-ssk ()
-  "Loads the SwiftStarterKits system and starts the server for development." 
-  (ql:quickload "starterkit-web")
-  (if (not (starterkit-web:running?))
-      (starterkit-web:start :port 8080))
-  (in-package :starterkit-web))
+;; The skyetools package, typically located as part
+;; of the universal common-lisp package at '~/dev/common-lisp/skyetools'
+(ql:quickload :skyetools)
 
-;; todo: dynamically add projects to local-projects
-;; todo: save this file in dotfiles
 ;; todo: add this file as part of new machine setup
-
-
-(in-package :cl-user)
